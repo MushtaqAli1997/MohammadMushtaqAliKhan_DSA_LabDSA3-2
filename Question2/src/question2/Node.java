9e4272a03e35a1f6ace20aeab38a00fb050e1235
MushtaqAli1997 <121460506+MushtaqAli1997@users.noreply.github.com>
2023-04-25 21:25:08 +0530

    Add files via upload

---
+++ Question2/src/question2/Node.java
@@ -0,0 +1,14 @@
+package question2;
+
+public class Node {
+	Node left;
+	int data;
+	Node right;
+
+	public Node(int data) {
+		this.left = null;
+		this.data = data;
+		this.right = null;
+	}
+
+}
